8f076a81c76697c0ac9a2f93d3fa3a02be28b1e5
ictseoyoungmin <90033184+ictseoyoungmin@users.noreply.github.com>
2025-12-03 22:19:29 +0900

    Refactor training flow into Trainer class

--- train.py
+++ train.py
@@ -2,19 +2,141 @@
 """
 defacto dataset으로 RRU-Net 네트워크 훈련 코드
 python train.py로 네트워크 훈련한 다음 RRU_train_test.ipynb 파일에서 결과 확인
-가까운 시일 내 predict.py로 성능평가 해야 함 
+가까운 시일 내 predict.py로 성능평가 해야 함
 """
 
-import torch.backends.cudnn as cudnn
-import torch
-from torch import nn
-from torch import optim
-from model.unet_model import Ringed_Res_Unet
-from dataset.Defacto import load_dataset
+import os
+import time
+
 import matplotlib.pyplot as plt
-import time,os
-# __________________________________________
+import torch
+import torch.backends.cudnn as cudnn
+from torch import nn, optim
+
+from dataset.Defacto import load_dataset
 from loss.dice_loss import dice_coeff
+from model.unet_model import Ringed_Res_Unet
+
+
+class Trainer:
+    def __init__(self, net, train_dataloader, val_dataloader, optimizer, criterion, device, dir_logs, dataset, lr, image_size, checkpoint=True):
+        self.net = net
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
+        self.dir_logs = dir_logs
+        self.dataset = dataset
+        self.lr = lr
+        self.image_size = image_size
+        self.checkpoint = checkpoint
+
+        self.train_losses = []
+        self.val_dices = []
+        self.epochs_run = []
+        self.spend_total_time = []
+
+        print(f'''
+    Starting training:
+        Batch size: {train_dataloader.batch_size}
+        Image size: {image_size}
+        Learning rate: {lr}
+        Training size: {len(train_dataloader)}
+        Validation size: {len(val_dataloader)}
+        Checkpoints: {str(checkpoint)}
+        CUDA: {str(device.type == "cuda")}
+    ''')
+
+    def train_one_epoch(self, epoch):
+        self.net.train()
+        epoch_loss = 0.0
+        start_epoch = time.time()
+        n_train = len(self.train_dataloader) * self.train_dataloader.batch_size
+
+        print('Starting epoch {}/{}.'.format(epoch + 1, self.total_epochs))
+        batch_idx = 0
+        for batch_idx, data in enumerate(self.train_dataloader, 1):
+            start_batch = time.time()
+            imgs = data['image'].to(self.device)
+            true_masks = data['landmarks'].to(self.device)
+
+            self.optimizer.zero_grad()
+
+            masks_pred = self.net(imgs)
+            masks_probs = torch.sigmoid(masks_pred)
+            masks_probs_flat = masks_probs.view(-1)
+            true_masks_flat = true_masks.view(-1)
+            loss = self.criterion(masks_probs_flat, true_masks_flat)
+
+            epoch_loss += loss.item()
+            loss.backward()
+            self.optimizer.step()
+
+            print('{:.4f} --- loss: {:.4f}, {:.3f}s'.format(batch_idx * self.train_dataloader.batch_size / n_train, loss, time.time() - start_batch))
+
+        mean_loss = epoch_loss / max(batch_idx, 1)
+        spend_per_time = time.time() - start_epoch
+        print('Epoch finished ! Loss: {:.4f}'.format(mean_loss))
+        print('Spend time: {:.3f}s'.format(spend_per_time))
+        self.spend_total_time.append(spend_per_time)
+        return mean_loss
+
+    def validate(self):
+        self.net.eval()
+        tot = 0.0
+        batch_count = 0
+
+        with torch.no_grad():
+            for batch_count, val in enumerate(self.val_dataloader, 1):  # 느려지면 imgs -> img
+                imgs = val['image'].to(self.device)
+                true_mask = val['landmarks'].to(self.device)
+
+                mask_pred = self.net(imgs)[0]
+                mask_pred = (torch.sigmoid(mask_pred) > 0.5).float()
+                tot += dice_coeff(mask_pred, true_mask).item()
+
+        val_dice = tot / max(batch_count, 1)
+        print('Validation Dice Coeff: {:.4f}'.format(val_dice))
+        return val_dice
+
+    def save_checkpoint(self, epoch, val_dice, train_loss):
+        if self.checkpoint and epoch < 140:
+            checkpoint_path = os.path.join(
+                self.dir_logs,
+                '{}-[val_dice]-{:.4f}-[train_loss]-{:.4f}-ep{}.pkl'.format(self.dataset, val_dice, train_loss, epoch + 15)
+            )
+            torch.save(self.net.state_dict(), checkpoint_path)
+
+    def log_metrics(self):
+        fig = plt.figure()
+        plt.title('Training Process')
+        plt.xlabel('epoch')
+        plt.ylabel('value')
+        l1, = plt.plot(self.epochs_run, self.train_losses, c='red')
+        l2, = plt.plot(self.epochs_run, self.val_dices, c='blue')
+
+        plt.legend(handles=[l1, l2], labels=['Tra_loss', 'Val_dice'], loc='best')
+        plt.savefig(self.dir_logs + 'Training Process for lr-{}.png'.format(self.lr), dpi=600)
+        plt.close()
+
+    def run(self, epochs):
+        self.total_epochs = epochs
+        for epoch in range(epochs):
+            train_loss = self.train_one_epoch(epoch)
+            val_dice = self.validate()
+
+            self.train_losses.append(train_loss)
+            self.val_dices.append(val_dice)
+            self.epochs_run.append(epoch)
+
+            self.log_metrics()
+            self.save_checkpoint(epoch, val_dice, train_loss)
+            print()
+
+        total_time = int(sum(self.spend_total_time))
+        print('Total time : {}m {}s'.format(total_time // 60, total_time % 60))
+
 
 def train_net(net,
               epochs=5,
@@ -25,129 +147,39 @@ def train_net(net,
               gpu=True,
               dataset=None,
               dir_logs=None,
-              dir_image=r'E:\splicing_1_img\img_jpg',
-              dir_mask = r"E:\splicing_1_annotations\probe_mask"
+              dir_image=r'E:\\splicing_1_img\\img_jpg',
+              dir_mask=r"E:\\splicing_1_annotations\\probe_mask"
               ):
-    # training images are square
-    # ids = split_ids(get_ids(dir_img))
-    # iddataset = split_train_val(ids, val_percent)
-
-    # splicing_1_img 개수는 10765
-    train_dataloader, val_dataloader = load_dataset(10764,
-                                                    img_size,
-                                                    batch_size,
-                                                    dir_img =  dir_image,
-                                                    dir_mask = dir_mask
+    train_dataloader, val_dataloader = load_dataset(
+        10764,
+        img_size,
+        batch_size,
+        dir_img=dir_image,
+        dir_mask=dir_mask
     )
 
-    print(f'''
-    Starting training:
-        Epochs: {epochs}
-        Batch size: {batch_size}
-        Image size: {img_size}
-        Learning rate: {lr}
-        Training size: {train_dataloader.__len__()}
-        Validation size: {val_dataloader.__len__()}
-        Checkpoints: {str(checkpoint)}
-        CUDA: {str(gpu)}
-    ''')
-    N_train = train_dataloader.__len__() * batch_size
+    device = torch.device('cuda' if gpu and torch.cuda.is_available() else 'cpu')
+    net.to(device)
 
-    optimizer = optim.Adam(net.parameters(),
-                           lr=lr)
+    optimizer = optim.Adam(net.parameters(), lr=lr)
     criterion = nn.BCELoss()
-    Train_loss  = []
-    Valida_dice = []
-    EPOCH = []
-    spend_total_time = []
 
-    for epoch in range(epochs):
-        net.train()
+    trainer = Trainer(
+        net=net,
+        train_dataloader=train_dataloader,
+        val_dataloader=val_dataloader,
+        optimizer=optimizer,
+        criterion=criterion,
+        device=device,
+        dir_logs=dir_logs,
+        dataset=dataset,
+        lr=lr,
+        image_size=img_size,
+        checkpoint=checkpoint
+    )
 
-        start_epoch = time.time()
-        print('Starting epoch {}/{}.'.format(epoch + 1, epochs))
-        # reset the generators
-        # train = get_imgs_and_masks(iddataset['train'], dir_img, dir_mask, img_scale, dataset)
-        # val = get_imgs_and_masks(iddataset['val'], dir_img, dir_mask, img_scale, dataset)
+    trainer.run(epochs)
 
-        epoch_loss = 0
-
-        for i, data in enumerate(train_dataloader,1):
-            start_batch = time.time()
-            imgs = data['image']
-            true_masks = data['landmarks']
-            # imgs = np.array([i[0] for i in b]).astype(np.float32)
-            # true_masks = np.array([i[1] for i in b]).astype(np.float32) / 255.
-
-            # imgs = torch.from_numpy(imgs)
-            # true_masks = torch.from_numpy(true_masks)
-
-            if gpu:
-                imgs = imgs.cuda()
-                true_masks = true_masks.cuda()
-
-            optimizer.zero_grad()
-
-            masks_pred = net(imgs)
-            masks_probs = torch.sigmoid(masks_pred)
-            masks_probs_flat = masks_probs.view(-1)
-            true_masks_flat = true_masks.view(-1)
-            loss = criterion(masks_probs_flat, true_masks_flat)
-
-            epoch_loss += loss.item()
-            loss.backward()
-            optimizer.step()
-
-            print('{:.4f} --- loss: {:.4f}, {:.3f}s'.format(i * batch_size / N_train, loss, time.time()-start_batch))
-
-        print('Epoch finished ! Loss: {:.4f}'.format(epoch_loss / i))
-
-        # validate the performance of the model
-        with torch.no_grad():
-            net.eval()
-            tot = 0.0
-
-            for i,val in enumerate(val_dataloader): #느려지면 imgs -> img
-                imgs = val['image']
-                true_mask = val['landmarks']
-               
-                if gpu:
-                    imgs = imgs.cuda()
-                    true_mask = true_mask.cuda()
-                    # tot.cuda()
-
-                mask_pred = net(imgs)[0]
-                mask_pred = (torch.sigmoid(mask_pred) > 0.5).float()
-                tot += dice_coeff(mask_pred, true_mask).item()
-
-            val_dice = tot / i
-        print('Validation Dice Coeff: {:.4f}'.format(val_dice))
-
-        Train_loss.append(epoch_loss / i)
-        Valida_dice.append(val_dice)
-        EPOCH.append(epoch)
-            
-        fig = plt.figure()
-        plt.title('Training Process')
-        plt.xlabel('epoch')
-        plt.ylabel('value')
-        l1, = plt.plot(EPOCH, Train_loss, c='red')
-        l2, = plt.plot(EPOCH, Valida_dice, c='blue')
-
-        plt.legend(handles=[l1, l2], labels=['Tra_loss', 'Val_dice'], loc='best')
-        plt.savefig(dir_logs + 'Training Process for lr-{}.png'.format(lr), dpi=600)
-        plt.close()
-
-        if epoch < 140:
-            torch.save(net.state_dict(),
-                   dir_logs + '{}-[val_dice]-{:.4f}-[train_loss]-{:.4f}-ep{}.pkl'.format(dataset, val_dice, epoch_loss / i,epoch+15))
-        spend_per_time = time.time() - start_epoch
-        print('Spend time: {:.3f}s'.format(spend_per_time))
-        spend_total_time.append(spend_per_time)
-        print()
-
-    Tt = int(sum(spend_total_time))    
-    print('Total time : {}m {}s'.format(Tt//60,Tt%60))
 
 def main():
     # config parameters
@@ -160,8 +192,8 @@ def main():
     dataset = "defactor" #'CASIA'
     model = 'Ringed_Res_Unet'
     dir_logs = './result/logs/{}/{}/'.format(dataset, model)
-    dir_image=r'E:\splicing_2_img\img_jpg'
-    dir_mask =  r"E:\splicing_2_annotations\probe_mask"
+    dir_image=r'E:\\splicing_2_img\\img_jpg'
+    dir_mask =  r"E:\\splicing_2_annotations\\probe_mask"
 
     # log directory 생성
     os.makedirs(dir_logs,exist_ok=True)
